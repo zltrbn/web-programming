92ca820628efef093ad2bb49ad20814cac89dbc5
zltrbn <144171244+zltrbn@users.noreply.github.com>
2025-12-12 02:38:57 +0300

    event update

--- js/event.js
+++ js/event.js
@@ -9,5 +9,61 @@ export function initUI() {
     requestGeolocation();
   }
 }
+export function renderAll() {
 
+  clearLocations();
+
+  if (state.currentLocation) {
+    const { card } = createCardForLocation(state.currentLocation, true);
+    selectors.locations.appendChild(card);
+    loadWeatherIntoCard(state.currentLocation, card);
+  }
+
+  state.otherCities.forEach((loc, idx) => {
+    const { card, removeBtn } = createCardForLocation(loc, false, idx);
+
+    if (removeBtn) {
+      removeBtn.addEventListener('click', () => {
+        state.otherCities.splice(idx, 1);
+        saveStateToStorage();
+        renderAll();
+      });
+    }
+
+    selectors.locations.appendChild(card);
+    loadWeatherIntoCard(loc, card);
+  });
+
+}
+
+
+export function requestGeolocation() {
+
+  selectors.status.textContent = "Запрашиваем доступ к геопозиции...";
+  if(!navigator.geolocation){
+    selectors.status.textContent = "Геолокация не поддерживается. Введите город вручную.";
+    showCityEntry();
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    pos => {
+      const lat = pos.coords.latitude;
+      const lon = pos.coords.longitude;
+      state.currentLocation = { type: "coords", lat, lon };
+      saveStateToStorage();
+      selectors.status.textContent = "Получаем прогноз...";
+      renderAll();
+    },
+    err => {
+      console.info("Ошибка геолокации:", err);
+      selectors.status.textContent = "Геолокация не получена. Введите город.";
+      showCityEntry();
+    }
+  );
+}
+
+function showCityEntry(){
+  selectors.cityInput.focus();
+}
 
